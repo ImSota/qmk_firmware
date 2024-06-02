ee9397db6ea7cb919d9686561aacdce3779a847a
ImSota <129357469+ImSota@users.noreply.github.com>
2024-06-02 14:10:48 +0900

    Create config.h

---
+++ keyboards/myky/default/config.h
@@ -0,0 +1,23 @@
+// Copyright 2024 Sota Horiguchi (@ImSota)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+//#define EE_HANDS
+#define MASTER_LEFT
+#define DYNAMIC_KEYMAP_LAYER_COUNT 7
+#define PRODUCT MyKy
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
